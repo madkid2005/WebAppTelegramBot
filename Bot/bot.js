04e4ec30cbfe42bbb25541f15a64feeada381d9c
Mahdi Hoseinian <162808972+madkid2005@users.noreply.github.com>
2024-06-12 04:59:30 -0700

    z

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,16 +1,20 @@
 const { Telegraf } = require("telegraf");
 
-const TOKEN = "7096004155:AAEjJe1oBwHdFxsnT8iAXfAJoJiJGsNNSsM";
+const TOKEN = "7249363758:AAHilIaSfqNAg4BtqZzE8uzW-Zh6GI6AJnI"; // جایگزین با توکن خود کنید
 const bot = new Telegraf(TOKEN);
 
 const web_link = "https://www.takkharidshop.ir/";
 
-bot.start((ctx) =>
-  ctx.reply("Welcome :)))))", {
+bot.start((ctx) => {
+  ctx.reply("آیا دنبال رلی نمیتونی پیدا کنی \n داش کیا اینجاست که برای فایل های مخصوص نشونت بده روی بازی کلیک کن تا بهت کمک کنه ", {
     reply_markup: {
-      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+      inline_keyboard: [
+        [
+          { text: "بازی با داش کیا", web_app: { url: web_link } },
+        ]
+      ]
     },
-  })
-);
+  });
+});
 
 bot.launch();
